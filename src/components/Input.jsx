3d2c46f3d5c9e9cc8e67c0b0c28cd07b9f908da9
Beto Toro <betotoro0902@gmail.com>
2023-02-14 16:06:02 -0500

    feat: create checkpoints logic

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,30 @@
-import { useState } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks"
 import ArrowIcon from '../icons/ArrowIcon.astro'
 import CopyIcon from '../icons/CopyIcon.astro'
+
+const PASSWORD_OPTIONS = {
+  0: {
+    type: 'uppercase',
+    isActive: false,
+  },
+  1: {
+    type: 'lowercase',
+    isActive: false
+  },
+  2: {
+    type: 'numbers',
+    isActive: false
+  },
+  3: {
+    type: 'symbols',
+    isActive: false
+  }
+}
 
 export const Input = () => {
   const [value, setValue] = useState(8)
   const [password, setPassword] = useState('')
+  const [option, setOption] = useState(PASSWORD_OPTIONS)
 
   const options = [
     'Include Uppercase Letters',
@@ -37,6 +57,18 @@ export const Input = () => {
     setPassword(passwordArray.join(''))
   }
 
+  const handleOptions = (indexOption) => {
+    let copyData = Object.assign({}, option)
+    let currentValue = copyData[indexOption]
+    currentValue = {
+      ...currentValue,
+      isActive: !currentValue.isActive
+    }
+    copyData[indexOption] = currentValue
+
+    setOption(copyData)
+  }
+
   return (
     <>
       <main class='text-center w-full'>
@@ -67,9 +99,12 @@ export const Input = () => {
           />
           <ul class='mt-8'>
             {
-              options.map((item) => (
+              options.map((item, index) => (
                 <li class='flex gap-6 mb-5 items-center'>
-                  <div class='w-5 h-5 border-2 border-white' />
+                  <div class='relative' onClick={() => handleOptions(index)}>
+                    <div class={`w-5 h-5 border-2  ${option[index].isActive ? 'bg-[var(--neon-green)] border-[var(--neon-green)]': 'border-white'}`} />
+                    <svg class='absolute top-1 left-[3px]' width="14" height="12" xmlns="http://www.w3.org/2000/svg"><path stroke="#18171F" stroke-width="3" fill="none" d="M1 5.607 4.393 9l8-8"/></svg>
+                  </div>
                   <p>{item}</p>
                 </li>
               ))
