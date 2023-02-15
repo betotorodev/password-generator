41aff83c9a51616f07da589f640de2ec2d1cdaad
Beto Toro <betotoro0902@gmail.com>
2023-02-15 09:41:05 -0500

    feat: add copy clipboard logic

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -85,6 +85,11 @@ export const Input = () => {
     setOption(copyData)
   }
 
+  const copyClipboard = () => {
+    navigator.clipboard.writeText(password)
+      .then(() => console.log('copy tex:', password))
+  }
+
   return (
     <>
       <main class='text-center w-full'>
@@ -93,7 +98,7 @@ export const Input = () => {
           class='w-full h-20 bg-[var(--dark-grey)] flex justify-between items-center py-5 px-8'
         >
           <h1 class='text-[32px]'>{password}</h1>
-          <svg width='21' height='24' xmlns='http://www.w3.org/2000/svg'>
+          <svg class='cursor-pointer' onClick={copyClipboard} width='21' height='24' xmlns='http://www.w3.org/2000/svg'>
           <path
             d='M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z'
             fill='#A4FFAF'></path>
@@ -142,12 +147,11 @@ export const Input = () => {
             </div>
           </section>
           <button
-            class='flex justify-center items-center gap-6 w-full bg-[var(--neon-green)] text-[var(--dark-grey)] text-lg py-5 mt-8'
+            class='border-2 border-transparent flex justify-center items-center gap-6 w-full bg-[var(--neon-green)] brightness-90 text-[var(--dark-grey)] text-lg py-5 mt-8 hover:brightness-100'
             onClick={getPassword}
             >GENERATE
             <svg width='12' height='12' xmlns='http://www.w3.org/2000/svg'>
               <path
-                fill='#24232C'
                 d='m5.106 12 6-6-6-6-1.265 1.265 3.841 3.84H.001v1.79h7.681l-3.841 3.84z'
               >
               </path>
