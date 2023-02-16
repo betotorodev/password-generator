701d3987225f11601cdf6f6b346e9a8e009c93bc
Beto Toro <betotoro0902@gmail.com>
2023-02-16 10:29:52 -0500

    feat: separate some logic from the main component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from "preact/hooks"
-import ArrowIcon from '../icons/ArrowIcon.astro'
-import CopyIcon from '../icons/CopyIcon.astro'
-import { copyClipboard } from "../utils/clipboard"
+import { usePassword } from "../hooks/usePassword"
+import { Copy } from "./icons"
+import { Range } from "./Range"
 import { Strength } from "./Strength"
 
-const PASSWORD_OPTIONS = {
-  0: {
-    type: 'uppercase',
-    isActive: false,
-  },
-  1: {
-    type: 'lowercase',
-    isActive: false
-  },
-  2: {
-    type: 'numbers',
-    isActive: false
-  },
-  3: {
-    type: 'symbols',
-    isActive: false
-  }
-}
-
 export const Input = () => {
-  const [value, setValue] = useState(8)
-  const [password, setPassword] = useState('password')
-  const [option, setOption] = useState(PASSWORD_OPTIONS)
-  const [error, setError] = useState('')
+  const { getPassword, handleOptions, password, error, option } = usePassword()
 
   const options = [
     'Include Uppercase Letters',
@@ -35,67 +12,6 @@ export const Input = () => {
     'Include Numbers',
     'Include Symbols'
   ]
-  
-  const numbers = `0123456789`
-  const characters = `!@#$%^&*()_+-=[]{}|;':",.<>/?`
-  const letters = `abcdefghijklmnopqrstuvwxyz`
-
-  const getPassword= () => {
-    if (!option[0].isActive && !option[1].isActive && !option[2].isActive && !option[3].isActive) {
-      setError('We need at least one option to generate your password')
-      return
-    } else {
-      const numbersArray = option[2].isActive ? numbers.split('') : []
-      const charactersArray = option[3].isActive ? characters.split('') : []
-      const lettersArray = !option[0].isActive && !option[1].isActive ? [] : letters.split('').map((letter, index) => {
-        if (option[0].isActive && option[1].isActive) {
-          let max = letters.length - 1
-          let min = 0 
-          const randomNumber = Math.floor((Math.random() * (max - min + 1)) + min);
-          if (randomNumber === index) {
-            return letter
-          } else {
-            return letter.toLocaleUpperCase()
-          }
-        } else if (option[0].isActive && !option[1].isActive) {
-          return letter.toLocaleUpperCase()
-        } else {
-          return letter
-        }
-      })
-      
-  
-      const arrayToIterate = [...numbersArray, ...charactersArray, ...lettersArray]
-      
-      const passwordArray = []
-      
-      for (let pass = 0; pass < value; pass++) {
-        let max = arrayToIterate.length - 1
-        let min = 0 
-        const randomNumber = Math.floor((Math.random() * (max - min + 1)) + min);
-  
-        passwordArray.push(arrayToIterate[randomNumber])
-      }
-  
-      setPassword(passwordArray.join(''))
-      setError('')
-    }
-  }
-
-  const handleOptions = (indexOption) => {
-    let copyData = Object.assign({}, option)
-    let currentValue = copyData[indexOption]
-    currentValue = {
-      ...currentValue,
-      isActive: !currentValue.isActive
-    }
-    copyData[indexOption] = currentValue
-
-    setOption(copyData)
-  }
-
-
-
 
   return (
     <>
@@ -105,26 +21,10 @@ export const Input = () => {
           class='w-full h-20 bg-[var(--dark-grey)] flex justify-between items-center py-5 px-8'
         >
           <h1 class='text-[32px]'>{password}</h1>
-          <svg class='cursor-pointer' onClick={() => copyClipboard({ password })} width='21' height='24' xmlns='http://www.w3.org/2000/svg'>
-          <path
-            d='M20.341 3.091 17.909.659A2.25 2.25 0 0 0 16.319 0H8.25A2.25 2.25 0 0 0 6 2.25V4.5H2.25A2.25 2.25 0 0 0 0 6.75v15A2.25 2.25 0 0 0 2.25 24h10.5A2.25 2.25 0 0 0 15 21.75V19.5h3.75A2.25 2.25 0 0 0 21 17.25V4.682a2.25 2.25 0 0 0-.659-1.591ZM12.469 21.75H2.53a.281.281 0 0 1-.281-.281V7.03a.281.281 0 0 1 .281-.281H6v10.5a2.25 2.25 0 0 0 2.25 2.25h4.5v1.969a.282.282 0 0 1-.281.281Zm6-4.5H8.53a.281.281 0 0 1-.281-.281V2.53a.281.281 0 0 1 .281-.281H13.5v4.125c0 .621.504 1.125 1.125 1.125h4.125v9.469a.282.282 0 0 1-.281.281Zm.281-12h-3v-3h.451c.075 0 .147.03.2.082L18.667 4.6a.283.283 0 0 1 .082.199v.451Z'
-            fill='#A4FFAF'></path>
-        </svg>
+          <Copy />
         </div>
         <section class='text-left mt-6 w-full bg-[var(--dark-grey)] py-5 px-8'>
-        <div class='flex justify-between items-center mb-4'>
-          <label class='text-lg' for='input-range'>Character Length</label>
-          <span class='text-[32px] text-[var(--neon-green)]'>{value}</span>
-        </div>
-        <input
-          onChange={(e) => setValue(e.target.value)}
-          type='range'
-          class='w-full'
-          id='input-range'
-          min='8'
-          max='18'
-          value={value}
-          />
+        <Range />
           <ul class='mt-8'>
             {
               options.map((item, index) => (
@@ -148,7 +48,7 @@ export const Input = () => {
               <div class='flex gap-2'>
                 {
                   [1, 2, 3, 4].map((item) => (
-                    <Strength item={item} strength={value}/>
+                    <Strength item={item} />
                   ))
                 }
               </div>
