f83fd9859a6286e2d85b279a5a20546002cd20a1
Beto Toro <betotoro0902@gmail.com>
2023-02-16 11:01:04 -0500

    feat: complete container handler

--- src/container/PasswordProject.jsx
+++ src/container/PasswordProject.jsx
@@ -1,10 +1,12 @@
 import { Input } from "../components/Input"
+import { PasswordManager } from "../components/PasswordManager"
 import { PasswordProvider } from '../context/passwordContext'
 
 export const PasswordProject = () => {
   return (
     <PasswordProvider>
       <Input />
+      <PasswordManager />
     </PasswordProvider>
   )
 }
