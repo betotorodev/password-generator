f9cdb9b2740d30ec531ad1aea1825113195b04be
Beto Toro <betotoro0902@gmail.com>
2023-02-16 10:30:31 -0500

    feat: implement context and simplify Strength component

--- src/components/Strength.jsx
+++ src/components/Strength.jsx
@@ -1,6 +1,8 @@
+import { useEffect } from "preact/hooks"
 import { useColor } from "../hooks/useColor"
+import { usePassword } from "../hooks/usePassword"
 
-export const Strength = ({ item, strength }) => {
-   const { color, level } = useColor({ strength })
+export const Strength = ({ item }) => {
+   const { color, level } = useColor()
    return <div class={`w-2.5 h-7 ${item > level ? 'border-2 border-white bg-transparent' : `border-0 ${color}`}`} />
  }
